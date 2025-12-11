33ca1af293d02ee5656882221a94df0c43340165
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-11 22:15:19 +0530

    navbar.js

---
+++ navbar.js
@@ -0,0 +1,10 @@
+export function navbar() {
+  return `
+    <nav style="background:black;padding:12px;color:white;display:flex;gap:20px;">
+      <a href="index.html" style="color:white;">Home</a>
+      <a href="signup.html" style="color:white;">Signup</a>
+      <a href="login.html" style="color:white;">Login</a>
+      <a href="todos.html" style="color:white;">Todos</a>
+    </nav>
+  `;
+}
