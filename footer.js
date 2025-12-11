cb50982d11ece312969ae8ec5fb091acc0144f26
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-11 22:18:44 +0530

    components

---
+++ footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+  return `
+    <footer style="margin-top:40px;padding:20px;text-align:center;background:#eee;">
+      <p>© 2025 My Modular App</p>
+    </footer>
+  `;
+}
